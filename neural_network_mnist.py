f9c9666cb734e25e50f5902b8231e3518c5b08b1
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2023-06-01 21:18:34 +0200

    fixed rprop_plus bug

--- neural_network_mnist.py
+++ neural_network_mnist.py
@@ -164,30 +164,38 @@ class Optimizer_RProp_Plus:
         self.__update_weights(layer)
         self.__update_biases(layer)
     def __update_weights(self, layer):
+        delta_w = np.copy(layer.delta_w_cache)
         same_sign = layer.dweights_cache * layer.dweights > 0
         layer.delta_weights[same_sign] = np.minimum(layer.delta_weights[same_sign] * self.positive_eta, self.delta_max)
+        delta_w[same_sign] = -np.sign(layer.dweights[same_sign]) * layer.delta_weights[same_sign]
+        layer.weights[same_sign] += delta_w[same_sign]
+
         different_sign = layer.dweights_cache * layer.dweights < 0
         layer.delta_weights[different_sign] = np.maximum(layer.delta_weights[different_sign] * self.negative_eta, self.delta_min)
-        delta_w = np.zeros(layer.weights.shape)
-        same_sign_update = layer.dweights_cache * layer.dweights >= 0
-        delta_w[same_sign_update] = -np.sign(layer.dweights[same_sign_update]) * layer.delta_weights[same_sign_update]
-        different_sign_update = layer.dweights_cache * layer.dweights < 0
-        delta_w[different_sign_update] = -layer.delta_w_cache[different_sign_update]
-        layer.dweights[different_sign_update] = 0
-        layer.weights += delta_w
+        layer.weights[different_sign] -= layer.delta_w_cache[different_sign]
+        layer.dweights[different_sign] = 0
+
+        zero_sign = layer.dweights_cache * layer.dweights == 0
+        delta_w[zero_sign] = -np.sign(layer.dweights[zero_sign]) * layer.delta_weights[zero_sign]
+        layer.weights[zero_sign] += delta_w[zero_sign]
+
         layer.delta_w_cache = delta_w
     def __update_biases(self, layer):
+        delta_b = np.copy(layer.delta_b_cache)
         same_sign = layer.dbiases_cache * layer.dbiases > 0
         layer.delta_biases[same_sign] = np.minimum(layer.delta_biases[same_sign] * self.positive_eta, self.delta_max)
+        delta_b[same_sign] = -np.sign(layer.dbiases[same_sign]) * layer.delta_biases[same_sign]
+        layer.biases[same_sign] += delta_b[same_sign]
+
         different_sign = layer.dbiases_cache * layer.dbiases < 0
         layer.delta_biases[different_sign] = np.maximum(layer.delta_biases[different_sign] * self.negative_eta, self.delta_min)
-        delta_b = np.zeros(layer.biases.shape)
-        same_sign_update = layer.dbiases_cache * layer.dbiases >= 0
-        delta_b[same_sign_update] = -np.sign(layer.dbiases[same_sign_update]) * layer.delta_biases[same_sign_update]
-        different_sign_update = layer.dbiases_cache * layer.dbiases < 0
-        delta_b[different_sign_update] = -layer.delta_b_cache[different_sign_update]  
-        layer.dbiases[different_sign_update] = 0     
-        layer.biases += delta_b
+        layer.biases[different_sign] -= layer.delta_b_cache[different_sign]
+        layer.dbiases[different_sign] = 0
+
+        zero_sign = layer.dbiases_cache * layer.dbiases == 0
+        delta_b[zero_sign] = -np.sign(layer.dbiases[zero_sign]) * layer.delta_biases[zero_sign]
+        layer.biases[zero_sign] += delta_b[zero_sign]
+
         layer.delta_b_cache = delta_b
 
 
@@ -488,7 +496,7 @@ def overlap_plots(values, epochs, xlabel, ylabel):
     plt.show()
 
 
-(train_X, train_y), (test_X, test_y) = get_dataset(10000, 2500)
+(train_X, train_y), (test_X, test_y) = get_dataset(100, 100)
 EPOCHS = 1000
 N_NEURONS = 64
 
@@ -504,15 +512,14 @@ print(f'TEST SET --- acc: {accuracy_test:.3f}, ' f'loss: {loss_test:.3f}')
 plot_values(epochs, loss_values, accuracy_values, "RProp_minus")
 '''
 
-'''
 epochs, loss_values, accuracy_values, loss_test, accuracy_test = execute_RProp_plus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS)
 print(f'TEST SET --- acc: {accuracy_test:.3f}, ' f'loss: {loss_test:.3f}')
 plot_values(epochs, loss_values, accuracy_values, "RProp_plus")
-'''
 
-epochs_iRProp_plus, loss_values_iRProp_plus, accuracy_values_iRProp_plus, loss_test_iRProp_plus, accuracy_test_iRProp_plus = execute_iRProp_plus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS)
-print(f'TEST SET --- acc: {accuracy_test_iRProp_plus:.3f}, ' f'loss: {loss_test_iRProp_plus:.3f}')
-plot_values(epochs_iRProp_plus, loss_values_iRProp_plus, accuracy_values_iRProp_plus, "iRProp_plus")
+
+#epochs_iRProp_plus, loss_values_iRProp_plus, accuracy_values_iRProp_plus, loss_test_iRProp_plus, accuracy_test_iRProp_plus = execute_iRProp_plus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS)
+#print(f'TEST SET --- acc: {accuracy_test_iRProp_plus:.3f}, ' f'loss: {loss_test_iRProp_plus:.3f}')
+#plot_values(epochs_iRProp_plus, loss_values_iRProp_plus, accuracy_values_iRProp_plus, "iRProp_plus")
 
 #epochs_iRProp_minus, loss_values_iRProp_minus, accuracy_values_iRProp_minus, loss_test_iRProp_minus, accuracy_test_iRProp_minus = execute_iRProp_minus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS)
 #print(f'TEST SET --- acc: {accuracy_test_iRProp_minus:.3f}, ' f'loss: {loss_test_iRProp_minus:.3f}')
