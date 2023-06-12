bdb7b0509b18f1272e4c10c1a308bb2f43757bd9
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2023-06-12 21:38:31 +0200

    edited xavier

--- run_all.py
+++ run_all.py
@@ -54,17 +54,19 @@ def thread_iRprop_plus(ret_container: dict, activ_func, weight_init):
     ret_container['accuracy_test'] = accuracy_test
 
 # TEST PARAMETERS
-(train_X, train_y), (test_X, test_y) = get_dataset(20000, 5000)
+(train_X, train_y), (test_X, test_y) = get_dataset(5000, 500)
 EPOCHS = 1000
-N_NEURONS = 64
+N_NEURONS = 150
 
-
+'''
 # RELU
-# activation1_sgd = Activation_ReLU()
-# activation1_rprop_minus = Activation_ReLU()
-# activation1_rprop_plus = Activation_ReLU()
-# activation1_irprop_plus = Activation_ReLU()
-# activation1_irprop_minus = Activation_ReLU()
+activation1_sgd = Activation_ReLU()
+activation1_rprop_minus = Activation_ReLU()
+activation1_rprop_plus = Activation_ReLU()
+activation1_irprop_plus = Activation_ReLU()
+activation1_irprop_minus = Activation_ReLU()
+'''
+
 
 # LEAKY RELU
 activation1_sgd = Activation_LReLU()
@@ -72,6 +74,7 @@ activation1_rprop_minus = Activation_LReLU()
 activation1_rprop_plus = Activation_LReLU()
 activation1_irprop_plus = Activation_LReLU()
 activation1_irprop_minus = Activation_LReLU()
+
 
 # SIGMOID
 # activation1_sgd = Activation_Sigmoid()
@@ -81,58 +84,58 @@ activation1_irprop_minus = Activation_LReLU()
 # activation1_irprop_minus = Activation_Sigmoid()
 
 # MULTI THREAD EXECUTION
-# ret_SGD = {}
-# ret_Rprop_minus = {}
-# ret_Rprop_plus = {}
-# ret_iRprop_minus = {}
-# ret_iRprop_plus = {}
-#
-# t1 = threading.Thread(target=thread_SGD, args=(ret_SGD, activation1_sgd, "random"))
-# t2 = threading.Thread(target=thread_Rprop_minus, args=(ret_Rprop_minus, activation1_rprop_minus, "random"))
-# t3 = threading.Thread(target=thread_Rprop_plus, args=(ret_Rprop_plus, activation1_rprop_plus, "random"))
-# t4 = threading.Thread(target=thread_iRprop_minus, args=(ret_iRprop_minus, activation1_irprop_minus, "random"))
-# t5 = threading.Thread(target=thread_iRprop_plus, args=(ret_iRprop_plus, activation1_irprop_plus, "random"))
-#
-# t1.start()
-# t2.start()
-# t3.start()
-# t4.start()
-# t5.start()
-#
-# t1.join()
-# t2.join()
-# t3.join()
-# t4.join()
-# t5.join()
-#
-# print(f'SGD TEST SET --- acc: {ret_SGD.get("accuracy_test"):.3f}, ' f'loss: {ret_SGD.get("loss_test"):.3f}')
-# print(f'Rprop- TEST SET --- acc: {ret_Rprop_minus.get("accuracy_test"):.3f}, ' f'loss: {ret_Rprop_minus.get("loss_test"):.3f}')
-# print(f'Rprop+ TEST SET --- acc: {ret_Rprop_plus.get("accuracy_test"):.3f}, ' f'loss: {ret_Rprop_plus.get("loss_test"):.3f}')
-# print(f'iRprop+ TEST SET --- acc: {ret_iRprop_plus.get("accuracy_test"):.3f}, ' f'loss: {ret_iRprop_plus.get("loss_test"):.3f}')
-# print(f'iRprop- TEST SET --- acc: {ret_iRprop_minus.get("accuracy_test"):.3f}, ' f'loss: {ret_iRprop_minus.get("loss_test"):.3f}')
-#
-# fig, axs = plt.subplots(2)
-# fig.suptitle("Grafici")
-# axs[0].plot(ret_SGD.get("epochs"), ret_SGD.get("loss"), label='SGD')
-# axs[0].plot(ret_Rprop_minus.get("epochs"), ret_Rprop_minus.get("loss"), label='Rprop-')
-# axs[0].plot(ret_Rprop_plus.get("epochs"), ret_Rprop_plus.get("loss"), label='Rprop+')
-# axs[0].plot(ret_Rprop_minus.get("epochs"), ret_iRprop_minus.get("loss"), label='iRprop-')
-# axs[0].plot(ret_iRprop_plus.get("epochs"), ret_iRprop_plus.get("loss"), label='iRprop+')
-#
-# axs[0].legend()
-#
-# axs[1].plot(ret_SGD.get("epochs"), ret_SGD.get("accuracy"), label='SGD')
-# axs[1].plot(ret_Rprop_minus.get("epochs"), ret_Rprop_minus.get("accuracy"), label='Rprop-')
-# axs[1].plot(ret_Rprop_plus.get("epochs"), ret_Rprop_plus.get("accuracy"), label='Rprop+')
-# axs[1].plot(ret_Rprop_minus.get("epochs"), ret_iRprop_minus.get("accuracy"), label='iRprop-')
-# axs[1].plot(ret_iRprop_plus.get("epochs"), ret_iRprop_plus.get("accuracy"), label='iRprop+')
-# axs.flat[0].set(xlabel='Epochs', ylabel='Loss')
-# axs.flat[1].set(xlabel='Epochs', ylabel='Accuracy')
-#
-# axs[1].legend()
-# plt.show()
+ret_SGD = {}
+ret_Rprop_minus = {}
+ret_Rprop_plus = {}
+ret_iRprop_minus = {}
+ret_iRprop_plus = {}
 
+t1 = threading.Thread(target=thread_SGD, args=(ret_SGD, activation1_sgd, "random"))
+t2 = threading.Thread(target=thread_Rprop_minus, args=(ret_Rprop_minus, activation1_rprop_minus, "random"))
+t3 = threading.Thread(target=thread_Rprop_plus, args=(ret_Rprop_plus, activation1_rprop_plus, "random"))
+t4 = threading.Thread(target=thread_iRprop_minus, args=(ret_iRprop_minus, activation1_irprop_minus, "random"))
+t5 = threading.Thread(target=thread_iRprop_plus, args=(ret_iRprop_plus, activation1_irprop_plus, "random"))
 
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+
+print(f'SGD TEST SET --- acc: {ret_SGD.get("accuracy_test"):.3f}, ' f'loss: {ret_SGD.get("loss_test"):.3f}')
+print(f'Rprop- TEST SET --- acc: {ret_Rprop_minus.get("accuracy_test"):.3f}, ' f'loss: {ret_Rprop_minus.get("loss_test"):.3f}')
+print(f'Rprop+ TEST SET --- acc: {ret_Rprop_plus.get("accuracy_test"):.3f}, ' f'loss: {ret_Rprop_plus.get("loss_test"):.3f}')
+print(f'iRprop+ TEST SET --- acc: {ret_iRprop_plus.get("accuracy_test"):.3f}, ' f'loss: {ret_iRprop_plus.get("loss_test"):.3f}')
+print(f'iRprop- TEST SET --- acc: {ret_iRprop_minus.get("accuracy_test"):.3f}, ' f'loss: {ret_iRprop_minus.get("loss_test"):.3f}')
+
+fig, axs = plt.subplots(2)
+fig.suptitle("Grafici")
+axs[0].plot(ret_SGD.get("epochs"), ret_SGD.get("loss"), label='SGD')
+axs[0].plot(ret_Rprop_minus.get("epochs"), ret_Rprop_minus.get("loss"), label='Rprop-')
+axs[0].plot(ret_Rprop_plus.get("epochs"), ret_Rprop_plus.get("loss"), label='Rprop+')
+axs[0].plot(ret_Rprop_minus.get("epochs"), ret_iRprop_minus.get("loss"), label='iRprop-')
+axs[0].plot(ret_iRprop_plus.get("epochs"), ret_iRprop_plus.get("loss"), label='iRprop+')
+
+axs[0].legend()
+
+axs[1].plot(ret_SGD.get("epochs"), ret_SGD.get("accuracy"), label='SGD')
+axs[1].plot(ret_Rprop_minus.get("epochs"), ret_Rprop_minus.get("accuracy"), label='Rprop-')
+axs[1].plot(ret_Rprop_plus.get("epochs"), ret_Rprop_plus.get("accuracy"), label='Rprop+')
+axs[1].plot(ret_Rprop_minus.get("epochs"), ret_iRprop_minus.get("accuracy"), label='iRprop-')
+axs[1].plot(ret_iRprop_plus.get("epochs"), ret_iRprop_plus.get("accuracy"), label='iRprop+')
+axs.flat[0].set(xlabel='Epochs', ylabel='Loss')
+axs.flat[1].set(xlabel='Epochs', ylabel='Accuracy')
+
+axs[1].legend()
+plt.show()
+
+'''
 # SINGLE THREAD EXECUTION
 epochs_SGD, loss_values_SGD, accuracy_values_SGD, loss_test_SGD, accuracy_test_SGD = execute_SGD(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS, activation1_sgd, "xavier")
 epochs_Rpropmin, loss_values_Rpropmin, accuracy_values_Rpropmin, loss_test_Rpropmin, accuracy_test_Rpropmin = execute_RProp_minus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS, activation1_rprop_minus, "xavier")
@@ -191,3 +194,4 @@ plt.show()
 # epochs_iRProp_minus, loss_values_iRProp_minus, accuracy_values_iRProp_minus, loss_test_iRProp_minus, accuracy_test_iRProp_minus = execute_iRProp_minus(train_X, train_y, test_X, test_y, EPOCHS, N_NEURONS)
 # print(f'TEST SET --- acc: {accuracy_test_iRProp_minus:.3f}, ' f'loss: {loss_test_iRProp_minus:.3f}')
 # plot_values(epochs_iRProp_minus, loss_values_iRProp_minus, accuracy_values_iRProp_minus, "iRProp_minus")
+'''
